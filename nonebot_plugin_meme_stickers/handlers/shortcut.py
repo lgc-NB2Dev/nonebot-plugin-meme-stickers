354769d482fb8158e73258f834335cb9974a9b25
student_2333 <lgc2333@126.com>
2025-02-20 17:02:32 +0800

    up

--- nonebot_plugin_meme_stickers/handlers/shortcut.py
+++ nonebot_plugin_meme_stickers/handlers/shortcut.py
@@ -1,5 +1,3 @@
-from arclet.alconna import command_manager
-from cookit.loguru import warning_suppress
 from nonebot import logger
 
 from ..sticker_pack import pack_manager
@@ -14,26 +12,28 @@ registered_commands: dict[str, set[str]] = {}
 def reregister_shortcuts(_: StickerPackManager, pack: StickerPack):
     pack_available = not pack.unavailable
     registered = registered_commands.get(pack.slug)
-    new_commands = set(
-        *pack.merged_config.commands,
-        *pack.merged_config.extend_commands,
+    new_commands = (
+        {
+            *pack.merged_config.commands,
+            *pack.merged_config.extend_commands,
+        }
+        if pack_available
+        else None
     )
 
     logger.debug(
         f"Pack `{pack.slug}` state changed, reregistering shortcuts"
-        f" (current state {'' if pack_available else 'un'}available"
-        f", registered {registered}"
-        f", new {new_commands})",
+        f" (`{registered}` -> `{new_commands}`)",
     )
 
     if registered:
         for x in registered:
-            with warning_suppress(f"Failed to delete shortcut {x}"):
-                command_manager.delete_shortcut(alc, x)
+            msg = alc.shortcut(x, delete=True)
+            logger.debug(msg)
         del registered_commands[pack.slug]
 
-    if not pack.unavailable:
+    if new_commands:
         for x in new_commands:
-            with warning_suppress(f"Failed to register shortcut {x}"):
-                alc.shortcut(x, arguments=["generate", pack.slug])
+            msg = alc.shortcut(x, arguments=["generate", pack.slug], prefix=True)
+            logger.debug(msg)
         registered_commands[pack.slug] = new_commands
